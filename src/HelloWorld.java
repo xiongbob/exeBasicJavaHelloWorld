fddfb9f267149d9003e193e805b0bbeded850ff8
xiongbob <xiong.bob@gmail.com>
2019-06-12 11:03:10 -0400

    commit HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Today is July 19, 2015. I am alive!");
+    }
+}
